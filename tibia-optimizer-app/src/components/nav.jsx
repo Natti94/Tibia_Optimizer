d135009c296b89ddafa52840e1ab5a4786d6cdfb
Natti94 <natnael.berhane.sv@gmail.com>
2025-08-31 10:21:31 +0200

    added video background at nav

--- tibia-optimizer-app/src/components/nav.jsx
+++ tibia-optimizer-app/src/components/nav.jsx
@@ -5,44 +5,63 @@ function Nav() {
   const [collapsed, setCollapsed] = useState(false);
 
   return (
-    <nav className={`side-nav${collapsed ? " collapsed" : ""}`}>
-      <button
-        className="side-nav-collapse-btn"
-        onClick={() => setCollapsed((v) => !v)}
-        aria-label={collapsed ? "Expand navigation" : "Collapse navigation"}
-        type="button"
-      >
-        <span className="side-nav-collapse-icon" color="">
-          {collapsed ? "»" : "«"}
-        </span>
-      </button>
-      <div className="side-nav-title">{!collapsed}</div>
-      <ul>
-        {!collapsed && (
-          <>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/donate">Guides</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/contact">Contact</Link>
-            </li>
-            <br />
-            <li>
-              <Link to="/support">Support Us</Link>
-            </li>
-          </>
-        )}
-      </ul>
-    </nav>
+    <div>
+      <nav className={`side-nav${collapsed ? " collapsed" : ""}`}>
+        <img
+          className="side-nav-image-top"
+          src="/nav/nav_image_top.gif"
+          alt="Nav Image"
+        />
+        <button
+          className="side-nav-collapse-btn"
+          onClick={() => setCollapsed((v) => !v)}
+          aria-label={collapsed ? "Expand navigation" : "Collapse navigation"}
+          type="button"
+        >
+          <span className="side-nav-collapse-icon" color="">
+            {collapsed ? "»" : "«"}
+          </span>
+        </button>
+        <div className="side-nav-title">{!collapsed}</div>
+        <ul>
+          {!collapsed && (
+            <>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <br />
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+              <br />
+              <li>
+                <Link to="/guides">Guides</Link>
+              </li>
+              <br />
+              <li>
+                <Link to="/cooperations">Cooperations</Link>
+              </li>
+              <br />
+              <li>
+                <Link to="/contact">Contact</Link>
+              </li>
+              <br />
+              <li>
+                <Link to="/support">Support Us</Link>
+              </li>
+            </>
+          )}
+          <video
+            className="side-nav-video-bottom"
+            src="/nav/nav_video_bottom.mp4"
+            alt="Nav Video"
+            autoPlay
+            loop
+            muted
+          />
+        </ul>
+      </nav>
+    </div>
   );
 }
 
